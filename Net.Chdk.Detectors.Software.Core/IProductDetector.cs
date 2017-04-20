80a2935f696869de74274a31b98576942e672bb9
Dmitry Shechtman <damnian@gmail.com>
2017-04-20 17:51:23 +0300

    Add solution

---
+++ Net.Chdk.Detectors.Software.Core/IProductDetector.cs
@@ -0,0 +1,9 @@
+﻿using Net.Chdk.Model.Software;
+
+namespace Net.Chdk.Detectors.Software
+{
+    public interface IProductDetector
+    {
+        ProductInfo GetProduct(string driveLetter);
+    }
+}
---
+++ Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
@@ -0,0 +1,9 @@
+﻿using Net.Chdk.Model.Software;
+
+namespace Net.Chdk.Detectors.Software
+{
+    public interface ISoftwareDetector
+    {
+        SoftwareInfo GetSoftware(string driveLetter);
+    }
+}
