a9feea939e42d24ed3bbc903640bd395126cb4f7
Dmitry Shechtman <damnian@gmail.com>
2017-05-10 01:38:30 +0300

    Add IInnerSoftwareDetector

---
+++ src/Net.Chdk.Detectors.Software.Core/IInnerSoftwareDetector.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Model.Card;
+using Net.Chdk.Model.Software;
+
+namespace Net.Chdk.Detectors.Software
+{
+    public interface IInnerSoftwareDetector
+    {
+        SoftwareInfo GetSoftware(CardInfo cardInfo);
+    }
+}
