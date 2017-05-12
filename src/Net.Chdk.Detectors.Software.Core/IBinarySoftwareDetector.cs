77c0c1f02863c348ef864ffe8e4cd13780e586ea
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 01:15:10 +0300

    Add IBinarySoftwareDetector

---
+++ src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
@@ -0,0 +1,14 @@
+﻿using Net.Chdk.Model.Software;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Chdk.Detectors.Software
+{
+    public interface IBinarySoftwareDetector
+    {
+        SoftwareInfo GetSoftware(SoftwareProductInfo product, SoftwareCameraInfo camera, byte[] buffer);
+    }
+}
