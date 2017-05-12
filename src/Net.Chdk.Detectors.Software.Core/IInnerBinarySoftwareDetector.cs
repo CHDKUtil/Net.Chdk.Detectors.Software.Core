25455d0cfafa3b1a9a639f10197c57071f6838c7
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 01:15:19 +0300

    Add ProductName

--- src/Net.Chdk.Detectors.Software.Core/IInnerBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IInnerBinarySoftwareDetector.cs
@@ -5,6 +5,7 @@ namespace Net.Chdk.Detectors.Software
 {
     public interface IInnerBinarySoftwareDetector
     {
+        string ProductName { get; }
         IEnumerable<byte[]> Bytes { get; }
         SoftwareInfo GetSoftware(byte[] buffer, int index);
     }
