1bbc6b001de3b3fc848ef3768546a977a94d2b2e
Dmitry Shechtman <damnian@gmail.com>
2017-05-13 18:13:36 +0300

    Remove unused usings

--- src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
@@ -1,9 +1,4 @@
 ﻿using Net.Chdk.Model.Software;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Net.Chdk.Detectors.Software
 {
