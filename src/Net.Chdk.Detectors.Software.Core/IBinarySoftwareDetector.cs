67c8122eaf3c654194e300d6c73ffee7de958520
Dmitry Shechtman <damnian@gmail.com>
2017-05-15 17:02:26 +0300

    Add progress

--- src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
@@ -1,10 +1,11 @@
 ﻿using Net.Chdk.Model.Software;
+using System;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface IBinarySoftwareDetector
     {
-        SoftwareInfo GetSoftware(string basePath);
+        SoftwareInfo GetSoftware(string basePath, IProgress<double> progress);
         SoftwareInfo UpdateSoftware(SoftwareInfo software, byte[] buffer);
     }
 }
--- src/Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
@@ -1,10 +1,11 @@
 ﻿using Net.Chdk.Model.Card;
 using Net.Chdk.Model.Software;
+using System;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface ISoftwareDetector
     {
-        SoftwareInfo GetSoftware(CardInfo cardInfo);
+        SoftwareInfo GetSoftware(CardInfo cardInfo, IProgress<double> progress);
     }
 }
