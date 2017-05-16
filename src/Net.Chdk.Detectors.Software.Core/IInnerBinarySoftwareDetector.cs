bd769335314dfd65f0c5ecce668fa088c4f22e7f
Dmitry Shechtman <damnian@gmail.com>
2017-05-17 02:46:51 +0300

    Add IInnerBinarySoftwareDetector

---
+++ src/Net.Chdk.Detectors.Software.Core/IInnerBinarySoftwareDetector.cs
@@ -0,0 +1,12 @@
+﻿using Net.Chdk.Model.Software;
+using System;
+using System.Threading;
+
+namespace Net.Chdk.Detectors.Software
+{
+    public interface IInnerBinarySoftwareDetector
+    {
+        SoftwareInfo GetSoftware(string basePath, IProgress<double> progress, CancellationToken token);
+        bool UpdateSoftware(ref SoftwareInfo software, byte[] buffer);
+    }
+}
