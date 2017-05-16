60845ed91fca02733c45c4f21e416548fd5719a6
Dmitry Shechtman <damnian@gmail.com>
2017-05-16 18:32:54 +0300

    Add cancellation

--- src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
@@ -1,11 +1,12 @@
 ﻿using Net.Chdk.Model.Software;
 using System;
+using System.Threading;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface IBinarySoftwareDetector
     {
-        SoftwareInfo GetSoftware(string basePath, IProgress<double> progress);
+        SoftwareInfo GetSoftware(string basePath, IProgress<double> progress, CancellationToken token);
         SoftwareInfo UpdateSoftware(SoftwareInfo software, byte[] buffer);
     }
 }
