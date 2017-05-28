83ff0a820594bd5edd81ca5294123d1980a06aac
Dmitry Shechtman <damnian@gmail.com>
2017-05-28 19:46:30 +0300

    IEnumerable<SoftwareInfo> GetSoftware()

--- src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IBinarySoftwareDetector.cs
@@ -1,12 +1,13 @@
 ﻿using Net.Chdk.Model.Software;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface IBinarySoftwareDetector
     {
-        SoftwareInfo GetSoftware(string basePath, IProgress<double> progress, CancellationToken token);
+        IEnumerable<SoftwareInfo> GetSoftware(string basePath, IProgress<double> progress, CancellationToken token);
         bool UpdateSoftware(SoftwareInfo software, byte[] buffer);
     }
 }
--- src/Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
@@ -1,12 +1,13 @@
 ﻿using Net.Chdk.Model.Card;
 using Net.Chdk.Model.Software;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface ISoftwareDetector
     {
-        SoftwareInfo GetSoftware(CardInfo cardInfo, IProgress<double> progress, CancellationToken token);
+        IEnumerable<SoftwareInfo> GetSoftware(CardInfo cardInfo, IProgress<double> progress, CancellationToken token);
     }
 }
