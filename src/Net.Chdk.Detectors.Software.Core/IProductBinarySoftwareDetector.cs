aeb51df996c555b0c5c9a80b5b71cfa3cec866c3
Dmitry Shechtman <damnian@gmail.com>
2017-05-28 15:06:35 +0300

    byte[] Bytes

--- src/Net.Chdk.Detectors.Software.Core/IProductBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IProductBinarySoftwareDetector.cs
@@ -1,12 +1,11 @@
 ﻿using Net.Chdk.Model.Software;
-using System.Collections.Generic;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface IProductBinarySoftwareDetector
     {
         string ProductName { get; }
-        IEnumerable<byte[]> Bytes { get; }
+        byte[] Bytes { get; }
         SoftwareInfo GetSoftware(byte[] buffer, int index);
     }
 }
