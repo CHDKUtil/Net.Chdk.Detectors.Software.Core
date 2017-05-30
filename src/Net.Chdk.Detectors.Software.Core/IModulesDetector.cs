0dfb4a8f5d1eef4a5cd38c57a440f42d3fa61541
Dmitry Shechtman <damnian@gmail.com>
2017-05-30 14:57:20 +0300

    Add progress

--- src/Net.Chdk.Detectors.Software.Core/IModulesDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IModulesDetector.cs
@@ -1,10 +1,11 @@
 ﻿using Net.Chdk.Model.Card;
 using Net.Chdk.Model.Software;
+using System;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface IModulesDetector
     {
-        ModulesInfo GetModules(CardInfo card, SoftwareInfo software);
+        ModulesInfo GetModules(CardInfo card, SoftwareInfo software, IProgress<double> progress);
     }
 }
