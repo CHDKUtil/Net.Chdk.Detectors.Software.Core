933a0e3da1b7a038dd8d3fdb634934e6058468af
Dmitry Shechtman <damnian@gmail.com>
2017-05-31 17:28:00 +0300

    Add cancellation

--- src/Net.Chdk.Detectors.Software.Core/IModulesDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IModulesDetector.cs
@@ -1,11 +1,12 @@
 ﻿using Net.Chdk.Model.Card;
 using Net.Chdk.Model.Software;
 using System;
+using System.Threading;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface IModulesDetector
     {
-        ModulesInfo GetModules(CardInfo card, SoftwareInfo software, IProgress<double> progress);
+        ModulesInfo GetModules(CardInfo card, SoftwareInfo software, IProgress<double> progress, CancellationToken token);
     }
 }
