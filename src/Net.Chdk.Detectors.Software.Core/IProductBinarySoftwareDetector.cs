c19e22d435ddbad49be97b8bbcdc0a6aa8a96567
Dmitry Shechtman <damnian@gmail.com>
2017-05-31 16:33:08 +0300

    Add CategoryName

--- src/Net.Chdk.Detectors.Software.Core/IProductBinarySoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/IProductBinarySoftwareDetector.cs
@@ -4,6 +4,7 @@ namespace Net.Chdk.Detectors.Software
 {
     public interface IProductBinarySoftwareDetector
     {
+        string CategoryName { get; }
         string ProductName { get; }
         byte[] Bytes { get; }
         SoftwareInfo GetSoftware(byte[] buffer, int index);
