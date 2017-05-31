e52f4aa96906406abad6355e284e86dd5fdee3d9
Dmitry Shechtman <damnian@gmail.com>
2017-05-31 16:21:05 +0300

    SoftwareInfo[] GetSoftware()

--- src/Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
+++ src/Net.Chdk.Detectors.Software.Core/ISoftwareDetector.cs
@@ -1,13 +1,12 @@
 ﻿using Net.Chdk.Model.Card;
 using Net.Chdk.Model.Software;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace Net.Chdk.Detectors.Software
 {
     public interface ISoftwareDetector
     {
-        IEnumerable<SoftwareInfo> GetSoftware(CardInfo cardInfo, IProgress<double> progress, CancellationToken token);
+        SoftwareInfo[] GetSoftware(CardInfo cardInfo, IProgress<double> progress, CancellationToken token);
     }
 }
